03d628e20de289508c7ba2f193b0e50444fd5a9a
hardsausage69 <98allenng@gmail.com>
2015-10-15 08:54:22 -0700

    pony distance edited

--- PonyDistance/PonyDistance.java
+++ PonyDistance/PonyDistance.java
@@ -4,32 +4,50 @@
  */
 public class PonyDistance
 {
+    //use of class constants in order to "simplify" but allows for more changeability for coordinates and such
+    public static final int BALTX = 31;
+    public static final int BALTY = 16;
+    public static final int MANEX = 34;
+    public static final int MANEY = 9;
+    public static final int LOSPX = 6;
+    public static final int LOSPY = 20;
+    public static final int NEIGHX = 22;
+    public static final int NEIGHY = 8;
+    public static final int BADLX = 25;
+    public static final int BADLY = 23;
+    public static final int PONYX = 16;
+    public static final int PONYY = 14;
+    //parameters used to call down below methods but many are used multiple times as they are related and stuff
     public static void main(String[] args)
     {
-        System.out.println("Distance from Baltimare to Manehatten: " + distance(31, 16, 34, 9));
-        System.out.println("Distance from Los Pegasus to Neighagra Falls: " + distance(6, 20, 22, 8));
-        System.out.println("Distance form Badlands to Ponyville: " + distance(25, 23, 16, 14));
-        System.out.println("Length of roadtrip: " + roadTrip(69));
-        System.out.println("Distance from City1 to City2: " + distance(31, 16, 6, 20));
-        System.out.println("Distance from City2 to City3: " + distance(6, 20, 25, 23));
-        System.out.println("Distance from City1 to City3: " + distance(31, 16, 25, 23));
-        System.out.println("Angle of City1: " + angle(31, 16, 6, 20, 25, 23));
-        System.out.println("Angle of City2: " + angle(6, 20, 31, 16, 25, 23));
-        System.out.println("Angle of City3: " + angle(25, 23, 31, 16, 6, 20));
-        System.out.println("Longest trip: " + longestTrip(31, 16, 6, 20, 25, 23));
+        System.out.println("Distance from Baltimare to Manehatten: " + distance(BALTX, BALTY, MANEX, MANEY));
+        System.out.println("Distance from Los Pegasus to Neighagra Falls: " + distance(LOSPX, LOSPY, NEIGHX, NEIGHY));
+        System.out.println("Distance form Badlands to Ponyville: " + distance(BADLX, BADLY, PONYX, PONYY));
+        System.out.println("Length of roadtrip: " + roadTrip(BALTX, BALTY, PONYX, PONYY));
+        System.out.println("Distance from City1 to City2: " + distance(BALTX, BALTY, LOSPX, LOSPY));
+        System.out.println("Distance from City2 to City3: " + distance(LOSPX, LOSPY, BADLX, BADLY));
+        System.out.println("Distance from City1 to City3: " + distance(BALTX, BALTY, BADLX, BADLY));
+        System.out.println("Angle of City1: " + angle(BALTX, BALTY, LOSPX, LOSPY, BADLX, BADLY));
+        System.out.println("Angle of City2: " + angle(LOSPX, LOSPY, BALTX, BALTY, BADLX, BADLY));
+        System.out.println("Angle of City3: " + angle(BADLX, BADLY, BALTX, BALTY, LOSPX, LOSPY));
+        System.out.println("Longest trip: " + longestTrip(BALTX, BALTY, LOSPX, LOSPY, BADLX, BADLY));
     }
+    //computes distance formula (Squareroot(x1 - x2)^2 + (y1 - y2)^2) with input as coordinates thus the x1 and y1
     public static double distance(int x1, int y1, int x2, int y2)
     {
         double c = (int)(100 * (Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y2 - y1, 2))));
         c = c / 100;
         return c;
     }
-    public static double roadTrip(int diameter)
+    //use of equation for circumference to find the length of the roadtrip: C = diameter * pi
+    public static double roadTrip(int x1, int y1, int x2, int y2)
     {
+        double diameter = distance(x1, y1, x2, y2);
         double length = (int) (100 * (Math.PI * diameter));
         length = length / 100;
         return length;
     }
+    //use of the law os cosine b/c we know all the coordinates thus all the distances; then we can isolate just the angle in the law of cosine which is what is utilized below
     public static double angle(int x1, int y1, int x2, int y2, int x3, int y3)
     {
         double a = distance(x1, y1, x2, y2);
@@ -39,6 +57,7 @@ public class PonyDistance
         d = d / 100;
         return d;
     }
+    //use of max utility for math to compare the lengths but because max can only 2 comparables we utilize the parentheses to compare the third distance with the larger of the first comparison
     public static double longestTrip(int x1, int y1, int x2, int y2, int x3, int y3)
     {
         double a = distance(x1, y1, x2, y2);
