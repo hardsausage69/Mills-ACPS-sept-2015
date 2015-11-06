7f9ac77037b14b9f80d9a90edbe8dec866c80632
hardsausage69 <98allenng@gmail.com>
2015-11-06 00:51:56 -0800

    wifey

--- InputOutput/InputOutput.java
+++ InputOutput/InputOutput.java
@@ -4,5 +4,25 @@
 import java.util.*;
 
 public class InputOutput{
-    Scanner inputStuff = new Scanner(System.in);
+    public static Scanner inputStuff = new Scanner(System.in);
+    public static void main(String[] args){
+        int deez = 5;
+        System.out.println("What's your name?");
+        String name = inputStuff.nextLine();
+        while (deez == 5){
+            System.out.println(name + ", will you go to homecoming with me(Allen)?");
+            System.out.println("Yes or No");
+            String answer = inputStuff.nextLine();
+            if (answer.equalsIgnoreCase("Yes")){
+                System.out.println("Aww thanks hun. Sorry this is really geeky and lame. I love you. <3");
+                deez = 3;
+            } else if (answer.equalsIgnoreCase("No")){
+                System.out.println("*sniffle* Why don't you love me? :'( *sniffle* I'm sorry; I'll try harder next time.");
+                deez = 3;
+            } else
+                System.out.println("That's not either yes or no. Try again.");
+        }
+    }
+}
+
     
