ee80ffdd273e60ab02d0dd347ab6c4df95e007a4
hardsausage69 <98allenng@gmail.com>
2016-02-03 21:27:13 -0800

    asdf

--- goolf/golfCard.java
+++ goolf/golfCard.java
@@ -3,18 +3,33 @@ import java.util.*;
 class golfCard{
     int persScore = 0;
     int parScore = 0;
+    
+    void calcScore(int holesIn){
+        Scanner console = new Scanner(System.in);
+        for (int hole = 0; hole < holesIn; hole++){
+            String golfIn = console.nextLine();
+            int par = parChange(golfIn);
+            int pers = persChange(golfIn);
+            compareParPers(hole, par, pers);
+            persScore = persScore + pers;
+            parScore = parScore + par;
+        }
+        int overall = parScore - persScore;
+        endResult(overall, holesIn);
+    }
+    
     void compareParPers(int hole,int par,int pers){
         int diff = par - pers;
         if (diff == 0){
-            System.out.println(hole + ". par");
+            System.out.println("par");
         } else if (diff == 1){
-            System.out.println(hole + ". birdie");
+            System.out.println("birdie");
         } else if (diff == 2){
-            System.out.println(hole + ". eagle");
+            System.out.println("eagle");
         } else if (diff == -1){
-            System.out.println(hole + ". bogey");
+            System.out.println("bogey");
         } else if (diff == -2){
-            System.out.println(hole + ". double bogey");
+            System.out.println("double bogey");
         } else {
             System.out.println("You're trash. Try harder.");
         }
@@ -22,7 +37,7 @@ class golfCard{
     
     int parChange(String input){
         int space = input.indexOf(' ',3);
-        String parStr = input.substring(3,space);
+        String parStr = input.substring(3, space);
         int par = Integer.parseInt(parStr);
         return par;
     }
@@ -30,8 +45,18 @@ class golfCard{
     int persChange(String input){
         input = input.trim();
         int space = input.indexOf(' ',3);
-        String persStr = input.substring(space,input.length());
+        String persStr = input.substring(space + 1);
         int pers = Integer.parseInt(persStr);
         return pers;
+    }
+    
+    void endResult(int overall, int holesIn){
+        if (overall == 0){
+            System.out.println("par");
+        } else if (overall > 0){
+            System.out.println(overall + " under par");
+        } else {
+            System.out.println(Math.abs(overall) + " over par");
+        }
     }
 }
--- goolf/golfCardInst.java
+++ goolf/golfCardInst.java
@@ -7,21 +7,6 @@ class golfCardInst{
         System.out.println("How many holes?");
         int holesIn = console.nextInt();
         System.out.println("Scores per hole? (<hole number>. <par> <your score>)");
-        for (int hole = 0; hole < holesIn; hole++){
-            String golfIn = console.nextLine();
-            int par = g.parChange(golfIn);
-            int pers = g.persChange(golfIn);
-            g.compareParPers(hole,par,pers);
-            g.persScore = g.persScore + pers;
-            g.parScore = g.parScore + par;
-        }
-        int overall = g.parScore - g.persScore;
-        if (overall == 0){
-            System.out.println(holesIn + ". par");
-        } else if (overall > 0){
-            System.out.println(holesIn + ". " + overall + "under par");
-        } else {
-            System.out.println(holesIn + ". " + Math.abs(overall) + "over par");
-        }
+        g.calcScore(holesIn);
     }
 }
